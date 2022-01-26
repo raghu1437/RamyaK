a4be7154e2543fce066f0a8e63689a2f626ce583
Ramya <ramyagvkn@gmail.com>
2022-01-26 07:39:33 +0530

    first commit

---
+++ Optional Parameters/Optional Parameters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Optional_Parameters
+{
+    class Program
+    {
+
+       static public void Employee(string Name, float Salary, int age = 25, string CompanyName = "Infosys")
+        {
+            Console.WriteLine("Employee Name : {0} Employee Salary: {1}  Employee age:{2} CompanyName: {3}", Name, Salary, age, CompanyName);
+            
+        }
+        static void Main(string[] args)
+        {
+            Employee("Ram", 10000);
+            Employee("Seetha", 20000);
+            Employee("Shyam", 30000);
+            Employee("Lakshmi", 10000);
+
+        }
+    }
+}
